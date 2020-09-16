f442b0ecaa45a68f5cfd27345e9359136aa4b638
GTMalogic <1306086395@qq.com>
2020-09-16 16:57:45 +0800

    Add files via upload

--- fire.py
+++ fire.py
@@ -59,7 +59,8 @@ def generate(path1):
          
 #每代改变温度
 def change_temp(t,it):
-    return t/(1+it)
+    global q
+    return t*q
 
 if __name__=="__main__":
     import numpy as np
@@ -79,34 +80,41 @@ if __name__=="__main__":
 
     
     #定义相关参数
-    t0=1e10
+    t0=1e5
     tend=1e-10
     #降温代数
-    it_total=10000
-    q=0.9
+    it_total=500
+    q=0.95
     #每代迭代次数
-    change=100
+    change=1000
     
     ####开始计算
     city_dis=cal_dis(city_loc)
     t=t0
     record=[]
+    temp_record=[]
     path1=randompath()
     for it in range(it_total):
         for c in range(change):
             path2=generate(path1)
             path1=cal_prob(path1,path2,t)
         t=change_temp(t,it)
+        temp_record.append(t)
         pathlength1,pathlength2=compare(path1,path2)
         record.append(pathlength1)
+        #打印每一代的最优解
         print(it,pathlength1)
     #打印
     final_path=path1
     print(final_path)
     print(pathlength1)
     import matplotlib.pyplot as plt
-    plt.plot(np.arange(1000),record[:1000])
-    #最优解是10628,我这个41341
+    plt.plot(np.arange(it_total),record)
+    plt.show()
+    plt.plot(np.arange(it_total),temp_record)
+    #最优解是10628,我这个36460
+    #这个算法如何调整好的退火温度方程很关键，如果温度下降慢了，可能不收敛，如果温度下降的够快，又无法探索到足够好的解
+    #除此之外，调整方法还可以是减少代数，增加每一代的探索次数但是我这样也只搞到了335966
     
         
             
